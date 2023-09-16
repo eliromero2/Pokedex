9194eac0044187d1dfab4b365d85a52a01b0f9ab
Tomas <talk.gtg@gmail.com>
2023-09-16 19:01:06 -0300

    Bootstrap styles

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once 'header.php';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Inicio</title>
 </head>
 <body>
@@ -20,5 +20,6 @@ include_once 'header.php';
     <input type="submit" value="Iniciar sesion">
 </form>
 <a href="cierroSesion.php">Cerrar sesion</a>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
