f84b19e2d119c70ddc092948ce395691a345f42d
eliromero2 <elianaromero002@gmail.com>
2023-09-19 20:34:25 -0300

    buscador en el index

---
+++ buscarPokemon.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'lib/mysql.php';
+include_once 'header.php';
+
+$db = new MySQLDatabase('localhost', 'root', '', 'pokedex');
+
+$busqueda = $_GET['busqueda'];
+
+$sql = "SELECT * FROM pokemon WHERE nombre LIKE '%$busqueda%' OR tipo LIKE '%$busqueda%'";
+
+$resultados = $db->fetchAll($sql);
+
+?>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">Numero</th>
+        <th scope="col">Imagen</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Descripcion</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    if (!empty($resultados)) {
+
+        foreach ($resultados as $pokemon) {
+            include 'components/tableRowSinBoton.php';
+        }
+
+
+    }
+    ?>
+
+    </tbody>
+</table>
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 include_once 'lib/mysql.php';
 include_once 'header.php';
+
+$db = new MySQLDatabase('localhost', 'root', '', 'pokedex');
+$pokemones = $db->fetchAll('select * from pokemon');
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,6 +26,37 @@ include_once 'header.php';
     <input type="submit" value="Iniciar sesion">
 </form>
 <a href="cierroSesion.php">Cerrar sesion</a>
+
+<div class="container">
+
+    <form class="d-flex my-5" role="search" method="GET" action="buscarPokemon.php">
+        <input class="form-control me-2" type="search" id="busqueda" name="busqueda">
+        <input class="btn btn-outline-success"  type="submit" value="Buscar">
+    </form>
+
+    <div class="row">
+    <div class="col">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Numero</th>
+                <th scope="col">Imagen</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripcion</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($pokemones as $pokemon){
+                include 'components/tableRowSinBoton.php';
+            }
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
