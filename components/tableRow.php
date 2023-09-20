24bfdcd6d5d81b2633ac703fa184a57b9d6e48d0
Tomas <talk.gtg@gmail.com>
2023-09-19 21:23:38 -0300

    Condicion para usar acciones en home

--- components/tableRow.php
+++ components/tableRow.php
@@ -7,8 +7,13 @@ $urlEdit = "/Pokedex/edit.php?id=".$pokemon["numero_identificador"];
   <td colspan="1"><img src=<?php echo $pokemon["imagen_path"] ?> width='40'></td>
   <td colspan="1"><a href="detallePokemon.php?nombre=<?php echo $pokemon["nombre"] ?>"><?php echo $pokemon["nombre"] ?></td>
   <td><?php echo $pokemon["descripcion"] ?></td>
-  <td class="d-flex ">
-    <a href=<?php echo $urlEdit; ?> class="btn btn-primary mx-2">Modificar</a>
+  
+  <?php 
+  if($_SESSION['usuario']){
+    echo '<td class="d-flex ">
+    <a href='.$urlEdit.' class="btn btn-primary mx-2">Modificar</a>
     <a href="" class="btn btn-danger">Borrar</a>
-  </td>
+  </td>';
+  }
+  ?>
 </tr>
