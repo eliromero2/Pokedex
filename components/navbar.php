53c0c288c3682798338c0654aecd6a4b0da400c5
Tomas <talk.gtg@gmail.com>
2023-09-24 18:52:33 -0300

    Arreglos para login

--- components/navbar.php
+++ components/navbar.php
@@ -3,7 +3,7 @@
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="index.php">
     <img src="./extras/logoPokemon.png" alt="" width="80" height="80">
 
     </a>
@@ -16,12 +16,12 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Inicio</a>
+          <a class="nav-link" href="index.php">Inicio</a>
         </li>
           <?php
           if (isset($_SESSION["usuario"])) {
               echo '<li class="nav-item">';
-              echo '  <a class="nav-link" href="../cierroSesion.php">Cerrar sesión</a>';
+              echo '  <a class="nav-link" href="cierroSesion.php">Cerrar sesión</a>';
               echo '</li>';
           }
           ?>
--- inicioSesion.php
+++ inicioSesion.php
@@ -3,34 +3,32 @@ include_once 'header.php';
 include_once 'lib/mysql.php';
 session_start();
 
-$username = isset( $_POST["usuario"])?$_POST["usuario"] : "";
-$password = isset( $_POST["contrasenia"])?$_POST["contrasenia"] : "";
+$username = isset($_POST["usuario"]) ? $_POST["usuario"] : null;
+$password = isset($_POST["contrasenia"]) ? $_POST["contrasenia"] : null;
 
 $db = new MySQLDatabase('localhost', 'root', '', 'pokedex');
 
 $sql = "SELECT * FROM users WHERE username = '$username'";
-$resultado = mysqli_query($db->getConnection(), $sql);
+$hasUser = $db->fetchOne($sql);
 
-if (mysqli_num_rows($resultado) == 1 && validarCampos($username,$password)) {
+if ($hasUser) {
 
-    // Usuario existe, verificar la contraseña
-    $row = mysqli_fetch_assoc($resultado);
-    $hashedPassword = $row["password"]; // Contraseña almacenada en la base de datos
+    $hashedPassword = $hasUser["password"]; // Contraseña almacenada en la base de datos
 
     if (md5($password) == $hashedPassword) { // Comparar el hash MD5 de la contraseña ingresada con la almacenada
         // Inicio de sesión exitoso
         $_SESSION["usuario"] = $username;
         header("Location: home.php");
-    } else {
-        echo'<div class="d-flex justify-content-center my-5">';
-        echo '<h3>Contraseña incorrecta. Verifica tus datos.</h3>';
-        echo'</div>';
-        echo'<div class="d-flex justify-content-center my-5">';
-        echo '<a class=" my-5 btn btn-outline-dark" href="loginForm.php">Volver al formulario</a>';
-        var_dump($password, $hashedPassword);
-        echo'</div>';
-    }
-} else {
+    } 
+}else {
+    echo'<div class="d-flex flex-column align-items-center justify-content-center my-5">';
+    echo '<h3>Credenciales incorrectas</h3>';
+    echo '<p>Verifica tus datos.</p>';
+    echo'</div>';
+    echo'<div class="d-flex justify-content-center my-5">';
+    echo '<a class=" my-5 btn btn-outline-dark" href="loginForm.php">Volver al formulario</a>';
+    echo'</div>';
+} /* else {
     // Usuario no existe, registrarlo en la base de datos
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
     if (mysqli_query($db->getConnection(), $sql)) {
@@ -41,21 +39,7 @@ if (mysqli_num_rows($resultado) == 1 && validarCampos($username,$password)) {
         echo "Error al registrar el usuario: " . mysqli_error($db->getConnection());
         header("Location: index.php");
     }
-}
-
-$db->disconnect();
-/*FRAGMENTO DE IF QUE FUNCIONA SIN HASH
- * // Usuario existe, verificar la contraseña
-$row = mysqli_fetch_assoc($resultado);
-if ($row["password"] == $password) {
-    // Inicio de sesión exitoso
-    $_SESSION["usuario"] = $username;
-    header("Location: home.php");
-} else {
-    // Contraseña incorrecta
-    echo "Contraseña incorrecta. Verifica tus credenciales.";
-}
-}*/
+} */
 
 function validarCampos($username, $password){
     return !empty($username) && !empty($password);
