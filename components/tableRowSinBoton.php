d42439150fde08f6f341dae6c3e71afc51203f80
Tomas <talk.gtg@gmail.com>
2023-09-24 18:09:04 -0300

    Imagenes de tipos

--- components/tableRowSinBoton.php
+++ components/tableRowSinBoton.php
@@ -1,12 +1,13 @@
 <?php
 $urlEdit = "/Pokedex/edit.php?id=".$pokemon["numero_identificador"];
+$urlImgType = "../imagenes/tipos/".$pokemon["tipo"];
 ?>
 
 <tr>
   <th scope="row"><?php echo $pokemon["numero_identificador"] ?></th>
   <td colspan="1"><img src=<?php echo $pokemon["imagen_path"] ?> width='40'></td>
   <td colspan="1"><?php echo $pokemon["nombre"] ?></td>
-  <td colspan="1"><img src=<?php echo $pokemon["tipo"] ?> width='40'></td>
+  <td colspan="1"><img src=<?php echo $urlImgType ?> width='40'></td>
   <td><?php echo $pokemon["descripcion"] ?></td>
 
 </tr>
