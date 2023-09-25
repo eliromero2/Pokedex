782c4fff8dda84fd581878275aff88e30212b502
lucasdbarrios <lucabarrios@alumno.unlam.edu.ar>
2023-09-25 10:15:16 -0300

    se agrego en el link de detalle en el index rowsinboton

--- components/tableRowSinBoton.php
+++ components/tableRowSinBoton.php
@@ -6,7 +6,7 @@ $urlImgType = "imagenes/tipos/".$pokemon["tipo"].".jpg";
 <tr>
   <th scope="row"><?php echo $pokemon["numero_identificador"] ?></th>
   <td colspan="1"><img src=<?php echo $pokemon["imagen_path"] ?> width='40'></td>
-  <td colspan="1"><?php echo $pokemon["nombre"] ?></td>
+  <td colspan="1"><a href="detallePokemon.php?nombre=<?php echo $pokemon["nombre"] ?>"><?php echo $pokemon["nombre"] ?></td>
   <td colspan="1"><img src=<?php echo $urlImgType ?> width='40'></td>
   <td><?php echo $pokemon["descripcion"] ?></td>
 
